6b161b88f2cf79fc0b17f23aefacb0ecd6123a94
yash <sawantyash13@gmail.com>
2024-06-03 03:09:07 -0400

    adding files for deplolyment

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template
+import pickle
+import nltk
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+app = Flask(__name__)
+
+def tokenizer(x): # custom tokenizer
+    return (
+        wnl.lemmatize(w) 
+        for w in word_tokenize(x) 
+        if len(w) > 2 and w.isalnum() # only words that are > 2 characters
+    )  
+
+import pickle
+with open('genrepredict.pkl', 'rb') as f:
+    classifier = pickle.load(f)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        lyrics = request.form['lyrics']
+        genre = predict_genre(lyrics)  # Assume this function will handle your ML model
+        return render_template('index.html', genre=genre)
+    return render_template('index.html')
+
+stop = list(set(stopwords.words('english'))) # stopwords
+wnl = WordNetLemmatizer() # lemmatizer
+
+
+def predict_genre(lyrics):
+    result = classifier.predict([lyrics])
+    return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
