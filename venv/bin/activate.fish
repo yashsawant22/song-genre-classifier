9d8d1ae813a48fb6d0ab4b435a1f7d53ed1cd32f
yash <sawantyash13@gmail.com>
2024-06-07 04:48:30 -0400

    updated scikit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/yashsawant/song_genre_predictor/venv"
+set -gx VIRTUAL_ENV "/Users/yashsawant/song-genre-classifier/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
